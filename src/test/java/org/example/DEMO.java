ff8089c25f303e6efaecc73b0f36b488fdcc9032
Dheeraj <sainidheeraj913@gmail.com>
2025-09-16 09:43:17 +0530

    normal changes

--- src/test/java/org/example/DEMO.java
+++ src/test/java/org/example/DEMO.java
@@ -4,5 +4,14 @@ public class DEMO {
 
     public static void main(String[] args) {
         System.out.println("It's crated to test the functionality only..");
+
+        System.out.println("Another test");
+        giveMessage();
     }
+
+    public static void giveMessage(){
+        System.out.println("This is just Hi from Demo project guy...");
+    }
+
+
 }
